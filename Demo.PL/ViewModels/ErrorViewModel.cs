664b5ea1e6e6403f546ab77e75a7ece784223247
MariamKhafagy <mariamkhafagy123@gmail.com>
2025-09-29 22:45:21 +0300

    Code

---
+++ Demo.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace Demo.PL.ViewModels
+
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
